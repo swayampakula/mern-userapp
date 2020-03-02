cbd695c2e52e97569ae79e2e9a328515360d0fc0
Ramesh N (Digital) <RA353444@wipro.com>
2020-03-02 11:37:21 +0530

    adding cors and using fetch in UserList.

--- app.js
+++ app.js
@@ -3,28 +3,19 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-// CORS Handling
-
-app.use(function(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With,  Content-Type, Accept"
-  // );
-  next();
-});
 
 mongoose
   .connect("mongodb://localhost:27017/sampleapp", {
--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
 // import SampleHigher from "../containers/hoc/SampleHigher";
 import UserDetails from "./UserDetails";
-const data = require("../data/data.json");
+// const data = require("../data/data.json");
 
 class UserList extends Component {
   state = {
     name: "",
-    person: []
+    person: [],
+    data: []
+  };
+
+  /*   componentDidMount = () => {
+    fetch("http://starlord.hackerearth.com/TopRamen")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  }; */
+
+  componentDidMount = () => {
+    fetch("http://localhost:3001/data/data.json")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        this.setState({ data: data });
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
   };
 
   render() {
@@ -14,20 +42,21 @@ class UserList extends Component {
       <React.Fragment>
         <div>
           {this.state.person.length === 0 &&
-            data.map((index, key) => {
-              console.log(key);
+            this.state.data.map((index, key) => {
               return (
                 <p
                   onClick={() =>
                     this.setState(
                       {
-                        person: data.filter(
+                        person: this.state.data.filter(
                           person => person.name === index.name
                         )
                       },
                       () => {
                         console.log(
-                          data.filter(person => person.name === index.name)
+                          this.state.data.filter(
+                            person => person.name === index.name
+                          )
                         );
                       }
                     )
